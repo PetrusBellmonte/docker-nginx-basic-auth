82d9e4493f9c373b43671b0a50f312971a0603f4
PetrusBellmonte <petrusbellmonte@gmail.com>
2025-10-11 08:21:34 +0200

    Clean escaping and improve safety

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
-FROM nginx:alpine
 
-ENV HTPASSWD='foo:$apr1$odHl5EJN$KbxMfo86Qdve2FH4owePn.' \
-    FORWARD_PORT=80 \
-    FORWARD_HOST=web
+
+FROM nginx:alpine
 
 WORKDIR /opt
 
--- launch.sh
+++ launch.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Check required environment variables
+: "${FORWARD_HOST:?FORWARD_HOST is not set}"
+: "${FORWARD_PORT:?FORWARD_PORT is not set}"
+: "${HTPASSWD:?HTPASSWD is not set}"
+
 rm /etc/nginx/conf.d/default.conf || :
-envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
-envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
+envsubst '$FORWARD_HOST $FORWARD_PORT' < auth.conf > /etc/nginx/conf.d/auth.conf
+envsubst '$HTPASSWD' < auth.htpasswd > /etc/nginx/auth.htpasswd
 
 exec nginx -g "daemon off;"
